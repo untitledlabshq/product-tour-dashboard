e590b6fed650b3f6ba22e87e485df1eb78de3a69
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-06-29 22:14:50 +0530

    ft: get projects

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -38,15 +38,12 @@ export default function Dashboard() {
         </div>
 
         <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 gap-3">
-          {projects}
-          {Array.from(new Array(10).keys()).map((n) => {
+          {projects.map((project: any) => {
             return (
-              <Link key={n} href="/project/1">
+              <Link key={project} href="/project/1">
                 <div className="border border-gray-600 bg-white dark:bg-neutral-600 p-5 rounded-lg">
-                  <h1 className="text-lg font-bold">Untitled Labs</h1>
-                  <p className="text-sm mt-1 gray-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </p>
+                  <h1 className="text-lg font-bold">{project.name}</h1>
+                  <p className="text-sm mt-1 gray-text">{project.desc}</p>
 
                   <p className="mt-3 pill">8 tours</p>
                 </div>
