aa8db01f0dd1f65a0a49c7eb5941f3eb2a37b315
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-06-29 20:27:15 +0530

    ft: new project working with api

--- components/ProjectDialog.tsx
+++ components/ProjectDialog.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -7,41 +7,92 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { API_URL } from "@/constants";
+import { useAppStore } from "@/store";
+import axios from "axios";
+import { useState } from "react";
 
 export default function ProjectDialog() {
+  const store = useAppStore();
+
+  const [open, setOpen] = useState(false);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    desc: "",
+  });
+
+  function handleChange(event: any) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+
+    await axios.post(API_URL + "/project", formData, {
+      headers: {
+        Authorization: "Bearer " + store.session.access_token,
+      },
+    });
+
+    setOpen(false);
+    setFormData({
+      name: "",
+      desc: "",
+    });
+  }
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={(value) => setOpen(value)}>
       <DialogTrigger asChild>
         <Button variant="secondary">+ New Project</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>New Project</DialogTitle>
-          <DialogDescription>
-            Each webapp is a project which contains multiple product tours for its pages
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="NFT Minting Site" className="col-span-3" />
+        <form onSubmit={handleSubmit}>
+          <DialogHeader>
+            <DialogTitle>New Project</DialogTitle>
+            <DialogDescription>
+              Each webapp is a project which contains multiple product tours for
+              its pages
+            </DialogDescription>
+          </DialogHeader>
+          <div onSubmit={handleSubmit} className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                name="name"
+                placeholder="NFT Minting Site"
+                className="col-span-3"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="desc" className="text-right">
+                desc
+              </Label>
+              <Input
+                id="desc"
+                name="desc"
+                className="col-span-3"
+                placeholder="BAYC Mint Page"
+                value={formData.desc}
+                onInput={handleChange}
+              />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description" className="text-right">
-              Description
-            </Label>
-            <Input id="description" value="BAYC Mint Page" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Create</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type="submit">Create</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
