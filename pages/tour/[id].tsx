e702003ea6e46642f0e3068b6217a57fec1a6087
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-07-18 17:46:17 +0530

    fix: click only on arrow to go back

--- pages/tour/[id].tsx
+++ pages/tour/[id].tsx
@@ -106,11 +106,8 @@ export default function TourId() {
         <main className="p-10">
           <div className="flex justify-between items-center">
             <div>
-              <div
-                className="flex items-center space-x-1 cursor-pointer"
-                onClick={() => window.history.back()}
-              >
-                <ArrowLeft width={24} />
+              <div className="flex items-center space-x-1">
+                <ArrowLeft width={24} onClick={() => window.history.back()} className="cursor-pointer" />
                 <div className="flex items-center space-x-3">
                   <h1 className="font-semibold text-2xl">{tour.name}</h1>
                   <p className="flex items-center space-x-2 text-xs mt-2 mb-1 py-2 px-3 pr-4 bg-gray-300 dark:bg-primary-light text-gray-400 rounded-full font-mono overflow-x-auto">
