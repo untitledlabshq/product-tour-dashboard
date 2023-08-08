284bbce12279f697c18efcd422ddd90549b20a36
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-08-08 12:00:18 +0530

    ft: no projects section

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,19 +6,22 @@ import axios from "axios";
 import Head from "next/head";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import elk from "@/assets/elk.svg";
 
 export default function Dashboard() {
   const store = useAppStore();
   const [projects, setProjects] = useState([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchProjects();
   }, [store.session]);
 
-  function fetchProjects() {
+  async function fetchProjects() {
     if (store.session) {
-      axios
+      setLoading(true);
+      await axios
         .get(API_URL + "/project/user/" + store.session.user.id, {
           headers: {
             Authorization: "Bearer " + store.session.access_token,
@@ -26,9 +29,11 @@ export default function Dashboard() {
         })
         .then((response) => {
           setProjects(response.data);
+          setLoading(false);
         })
         .catch((e) => {
           setError(e.message);
+          setLoading(false);
         });
     }
   }
@@ -46,7 +51,8 @@ export default function Dashboard() {
         </div>
 
         <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 gap-3">
-          {projects.length === 0 && <span>{error ? error : "Loading..."}</span>}
+          {loading && <span>Loading...</span>}
+          {projects.length === 0 && error && <span>{error}</span>}
           {projects.map((project: any) => {
             return (
               <Link key={project.id} href={"/project/" + project.id}>
@@ -71,6 +77,13 @@ export default function Dashboard() {
             );
           })}
         </div>
+        {!loading && projects.length === 0 && (
+          <div className="mt-10 space-y-4 grid place-items-center">
+            <img src={elk.src} alt="Elk" />
+            <h1 className="font-medium text-2xl">Start with new project</h1>
+            <ProjectDialog onCreate={fetchProjects} />
+          </div>
+        )}
       </div>
     </main>
   );
