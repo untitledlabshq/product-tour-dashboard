b79405117d1c3476ebcafd6fda27a4541e03dff6
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-09-28 17:37:38 +0530

    chore: update supabase config

--- utils/client.ts
+++ utils/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://pualfqvlljzjrdpsrcwy.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1YWxmcXZsbGp6anJkcHNyY3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcwMTEwMTcsImV4cCI6MjAwMjU4NzAxN30.v3JFuKE6MdoWZDUxW8jdIzYCnrFAJ0k_zuTooUGFNyk",
+  "https://gtgifkjpgvzlvczoozju.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0Z2lma2pwZ3Z6bHZjem9vemp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3MjQwNzcsImV4cCI6MjAxMTMwMDA3N30.N3WS9-t9FshfGWXV-9wo64Kdw34-ZLrVv-WBCBaAfuY",
   {
     auth: {
       persistSession: true,
