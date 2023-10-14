40270f70898f12cd04521f27f336856c4d6fb804
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-10-14 10:11:27 +0530

    fix: dashboard fetching if web2 session expired (no user)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -14,6 +14,7 @@ import { getEncryptedAddress } from "@/utils/crypto";
 import { createUser } from "@/utils/api";
 import { useRouter } from "next/router";
 import { useAccount } from "wagmi";
+import { useUser } from "@supabase/auth-helpers-react";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const { address } = await siweServer.getSession(req, res);
@@ -72,13 +73,18 @@ function Dashboard({
 
   const { isSignedIn } = useSIWE();
 
+  const user = useUser();
+
   const { status } = useAccount();
 
   useEffect(() => {
-    if (store?.session?.user?.id || (status !== "reconnecting" && isSignedIn)) {
+    if (
+      (user && store?.session?.user?.id) ||
+      (status !== "reconnecting" && isSignedIn)
+    ) {
       fetchProjects();
     }
-  }, [store.session, isSignedIn, status]);
+  }, [store.session, isSignedIn, status, user]);
 
   async function fetchProjects(refetch = false) {
     if (isSignedIn && projectsWeb3) {
@@ -91,7 +97,7 @@ function Dashboard({
       return;
     }
 
-    if (store.session.access_token) {
+    if (user && store.session.access_token) {
       try {
         setLoading(true);
 
@@ -121,59 +127,59 @@ function Dashboard({
 
   return (
     // <Layout>
-      <>
-        <Head>
-          <title>Dashboard</title>
-        </Head>
-        <Navbar />
-        <div className="pt-12 px-8 md:px-12">
-          <div className="flex justify-between items-center">
-            <h1 className="text-3xl">Your Projects</h1>
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <Navbar />
+      <div className="pt-12 px-8 md:px-12">
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl">Your Projects</h1>
+          <ProjectDialog
+            onCreate={() => fetchProjects(true)}
+            encryptedAddress={encryptedAddress}
+          />
+        </div>
+
+        <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 gap-3">
+          {loading && <span>Loading...</span>}
+          {projects.length === 0 && error && <span>{error}</span>}
+          {projects.map((project: any) => {
+            return (
+              <Link key={project.id} href={"/project/" + project.id}>
+                <div className="flex flex-col justify-between border border-gray-800 bg-white dark:bg-primary-purple p-5 rounded-lg h-full">
+                  <div>
+                    <h1 className="text-lg font-bold">{project.name}</h1>
+                    <p className="text-sm mt-1 gray-text">{project.desc}</p>
+                  </div>
+
+                  <div className="mt-3">
+                    {project.tour_count ? (
+                      <p className="pill">
+                        {project.tour_count} Tour
+                        {project.tour_count > 1 && "s"}
+                      </p>
+                    ) : (
+                      <p className="pill border-gray-500">No Tours</p>
+                    )}
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+        {!loading && projects.length === 0 && (
+          <div className="mt-10 space-y-4 grid place-items-center">
+            <img src={elk.src} alt="Elk" />
+            <h1 className="font-medium text-2xl">Start with new project</h1>
             <ProjectDialog
               onCreate={() => fetchProjects(true)}
               encryptedAddress={encryptedAddress}
             />
           </div>
-
-          <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 gap-3">
-            {loading && <span>Loading...</span>}
-            {projects.length === 0 && error && <span>{error}</span>}
-            {projects.map((project: any) => {
-              return (
-                <Link key={project.id} href={"/project/" + project.id}>
-                  <div className="flex flex-col justify-between border border-gray-800 bg-white dark:bg-primary-purple p-5 rounded-lg h-full">
-                    <div>
-                      <h1 className="text-lg font-bold">{project.name}</h1>
-                      <p className="text-sm mt-1 gray-text">{project.desc}</p>
-                    </div>
-
-                    <div className="mt-3">
-                      {project.tour_count ? (
-                        <p className="pill">
-                          {project.tour_count} Tour
-                          {project.tour_count > 1 && "s"}
-                        </p>
-                      ) : (
-                        <p className="pill border-gray-500">No Tours</p>
-                      )}
-                    </div>
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
-          {!loading && projects.length === 0 && (
-            <div className="mt-10 space-y-4 grid place-items-center">
-              <img src={elk.src} alt="Elk" />
-              <h1 className="font-medium text-2xl">Start with new project</h1>
-              <ProjectDialog
-                onCreate={() => fetchProjects(true)}
-                encryptedAddress={encryptedAddress}
-              />
-            </div>
-          )}
-        </div>
-      </>
+        )}
+      </div>
+    </>
     // </Layout>
   );
 }
